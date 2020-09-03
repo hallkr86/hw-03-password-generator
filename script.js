d71275b89d89741e25810eebb3f216c5c3a6c9be
Kesha Hall <k_renee10@yahoo.com>
2020-09-03 18:02:50 -0400

    created function

--- script.js
+++ script.js
@@ -3,12 +3,22 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = "";
   var passwordText = document.querySelector("#password");
+  var length = 8;
+  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  
 
+  for (var i = 0; i < length ; i++) {
+      password += charset.charAt(Math.floor(Math.random() * length));
+  
+  
   passwordText.value = password;
-
+  }
+  return password;
+  
 }
+ console.log(password);
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
